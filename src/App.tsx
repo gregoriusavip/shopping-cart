576b8a22aa6e953b72f46f5a6021a520ac9e8b53
Gregorius Avip <71865513+gregoriusavip@users.noreply.github.com>
2025-10-15 11:05:43 -0700

    Add styling and include header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import Header from "./components/Header";
+
 function App() {
   return (
-    <div>
-      <h1>Shopping Cart</h1>
-    </div>
-  )
+    <main className="px-16 py-[3rem] text-default-size font-montserrat font-medium">
+      <Header></Header>
+    </main>
+  );
 }
 
-export default App
+export default App;
