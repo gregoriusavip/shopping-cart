19a767b7f8ca91a29aee36bff3ad0e4cd9f6094d
Gregorius Avip <71865513+gregoriusavip@users.noreply.github.com>
2025-10-15 20:32:44 -0700

    Change padding

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Header from "./components/Header";
 
 function App() {
   return (
-    <main className="px-16 py-[3rem] text-default-size font-montserrat font-medium">
+    <main className="px-12 py-[3rem] text-default-size font-montserrat font-medium">
       <Header></Header>
     </main>
   );
